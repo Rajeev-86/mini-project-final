27b7f72a6b79987997f67f11c1b73d3492b83702
Rajeev <rajeev.86dev@gmail.com>
2025-05-04 18:06:19 +0530

    Update main.py

--- main.py
+++ main.py
@@ -65,8 +65,8 @@ uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png
 if uploaded_file is not None:
     file_bytes = np.asarray(bytearray(uploaded_file.read()), dtype=np.uint8)
     image = cv2.imdecode(file_bytes, 1)
+    st.image(image, caption="Uploaded Image", use_container_width=True)
 
-    st.image(image, caption="Uploaded Image", use_column_width=True)
 
     pose_vector = extract_pose_landmarks(image)
 
